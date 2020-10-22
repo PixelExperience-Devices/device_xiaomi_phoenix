7a3e75769e3e862074c124fec2cf29f7d9ed6854
derfelot <derfelot.git@gmail.com>
2020-10-22 04:52:06 +0200

    phoenix: Exclude CarrierConfig overlays from RRO
    They doesn't work right, because xml attributes where
    stripped down, and leading zeros are removed.
    
    E.g. instead of
     <carrier_config mcc="262" mnc="06">
    the values are modified and get
     <carrier_config mcc="262" mnc="6">
    
    This prevents matching of defined carrier definitions.
    
    Change-Id: Ib80ed6e4bab0014682d3be0f7d0e0a4faec8fa5c

--- device.mk
+++ device.mk
@@ -372,6 +372,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
