e626bdd32a8cc1897f5ab72d3c19e5c570ed2c81
Arian <arian.kulmer@web.de>
2021-06-18 18:41:53 +0200

    phoenix: power: Import powerhint configuration from Pixel 4A
    powerhint.json is checked out to https://android.googlesource.com/device/google/sunfish/+/refs/tags/android-11.0.0_r38/powerhint.json.
    
    Change-Id: Ib576cea82363865cefd5e5fa2e7c746c0b95c3a8

--- device.mk
+++ device.mk
@@ -421,6 +421,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
