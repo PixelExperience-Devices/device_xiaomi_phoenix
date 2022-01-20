884233cafc6adc64e865762143289ecc754f6a1a
jhenrique09 <jhsv09@gmail.com>
2022-01-20 17:22:10 +0000

    phoenix: Enable quick tap
    Change-Id: I86135ea3c4fb9063f5158f4f7ddd2d7e0f6b8062

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -6,7 +6,7 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
+# Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -16,9 +16,10 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
 
-# ArrowOS Properties
+# PixelExperience Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_USES_AOSP_RECOVERY := true
